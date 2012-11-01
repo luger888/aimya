0d4c4e169607cb9db6e75ae9185e3bef21a20f7f
luger888 <morbes77@gmail.com>
2012-11-01 18:08:41 +0200

    serviceDetailsForm

--- application/models/DbTable/Profile.php
+++ application/models/DbTable/Profile.php
@@ -44,4 +44,23 @@ class Application_Model_DbTable_Profile extends Application_Model_DbTable_Abstra
         $this->insert($data);
 
     }
+
+    public function addService($array, $id){
+
+        $data = array(
+
+            'user_id' => $id,
+            'lesson_category' => $array['lesson_category'],
+            'subcategory' => $array['subcategory'],
+            'rate' => (int)$array['rate'],
+            'duration' => $array['duration'],
+            'description' => $array['description'],
+            'created_at' => date('Y-m-d H:m:s'),
+            'updated_at' => date('Y-m-d H:m:s')
+
+        );
+
+        $this->insert($data);
+
+    }
 }
