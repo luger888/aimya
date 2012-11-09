2b4b3c1c92ce2a4606db99da60d6885beb16d164
luger888 <morbes77@gmail.com>
2012-11-09 15:02:40 +0200

    features filters form

---
+++ application/forms/FeaturesFilter.php
@@ -0,0 +1,31 @@
+<?php
+
+class Application_Form_FeaturesFilter extends Zend_Form
+{
+    public $basicDecorators = array('ViewHelper', 'Errors');
+    public $basicFilters = array('StripTags', 'StringTrim');
+
+    public function init()
+    {
+        $lessonDbModel = new Application_Model_DbTable_LessonCategory();
+        $lessonCategories = $lessonDbModel->getLessonCategories(); //category from db
+
+        $author = new Zend_Form_Element_Select('author');
+        $author->setAttrib('id', 'author')
+            ->addFilters($this->basicFilters)
+            ->setDecorators($this->basicDecorators)
+            ->addMultiOptions(array('2' => 'Instructors only',
+            '1' => 'Students only'
+        ));
+
+        $lesson_category = new Zend_Form_Element_Select('lesson_category');
+        $lesson_category->setAttrib('id', 'lesson_category')
+            ->addFilters($this->basicFilters)
+            ->setDecorators($this->basicDecorators)
+            ->addMultiOptions(array($lessonCategories[0]['title'] => $lessonCategories[0]['title']));
+
+        $this->addElements(array($author, $lesson_category));
+    }
+}
+
+?>
