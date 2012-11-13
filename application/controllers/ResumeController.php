a5cf0b0031c7d46626f3bb6443d4f891f1098443
luger888 <morbes77@gmail.com>
2012-11-13 16:56:04 +0200

    resume forms + views

--- application/controllers/ResumeController.php
+++ application/controllers/ResumeController.php
@@ -20,4 +20,36 @@ class ResumeController extends Zend_Controller_Action implements Aimya_Controlle
         $this->view->avatarPath = $profileModel->getAvatarPath($identity->id); //path to avatar
     }
 
+     public function objectiveAction()
+     {
+         $this->_helper->layout()->disableLayout();
+
+         $objectiveForm = new Application_Form_ResumeObjective();
+         $this->view->objectiveForm = $objectiveForm;
+     }
+
+    public function experienceAction()
+    {
+        $this->_helper->layout()->disableLayout();
+
+        $experienceForm = new Application_Form_ResumeExperience();
+        $this->view->experienceForm = $experienceForm;
+    }
+
+    public function educationAction()
+    {
+        $this->_helper->layout()->disableLayout();
+
+        $educationForm = new Application_Form_ResumeEducation();
+        $this->view->educationForm = $educationForm;
+    }
+
+    public function skillsAction()
+    {
+        $this->_helper->layout()->disableLayout();
+
+        $skillsForm = new Application_Form_ResumeSkills();
+        $this->view->skillsForm = $skillsForm;
+    }
+
 }
---
+++ application/forms/ResumeExperience.php
@@ -0,0 +1,53 @@
+<?php
+
+class Application_Form_ResumeExperience extends Zend_Form
+{
+    public $basicDecorators = array('ViewHelper', 'Errors');
+    public $basicFilters = array('StripTags', 'StringTrim');
+
+    public function init()
+    {
+        $identity =  Zend_Auth::getInstance()->getStorage()->read();
+        $this->setName('experience');
+        $this->setEnctype(Zend_Form::ENCTYPE_MULTIPART);
+
+        $experience = new Zend_Form_Element_Textarea('experience');
+        $experience ->setAttrib('id', 'experience')
+            ->addFilters($this->basicFilters)
+            ->setDecorators($this->basicDecorators)
+            ->setAttrib('rows', '7');
+
+        $delete = new Zend_Form_Element_Button('deleteExperience');
+        $delete ->setLabel('Delete')
+            ->setAttrib('id', 'deleteExperience')
+            ->setAttrib('class', 'btn')
+            ->setDecorators($this->basicDecorators);
+
+        $add = new Zend_Form_Element_Button('addExperience');
+        $add ->setLabel('Add')
+            ->setAttrib('id', 'addExperience')
+            ->setAttrib('class', 'btn')
+            ->setDecorators($this->basicDecorators);
+
+        $file = new Zend_Form_Element_File('file');
+        $file ->setLabel('Upload File')
+            ->setAttrib('id', 'file')
+            ->addValidator('Size', false, 1024000)
+            ->addValidator('Extension', false, 'jpg,png,gif,jpeg')
+            ->setDestination('./img/uploads/'.$identity->id.'/experience/');
+
+        $submit = new Zend_Form_Element_Submit('saveExperience');
+        $submit ->setLabel('Save')
+            ->setAttrib('id', 'saveExperience')
+            ->setAttrib('class', 'btn')
+            ->setDecorators($this->basicDecorators);
+
+
+
+
+        $this->addElements(array($experience, $delete, $add, $file, $submit));
+
+    }
+}
+
+?>
