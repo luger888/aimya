e92dfb3ea66e235ad7f8fe32d0ea4fbcc2fbeb69
Aleksandr Lozinskiy <alex.lozinskit@gmail.com>
2012-11-26 19:22:59 +0200

    finished friend functionality, revert cookie in main.js

--- application/models/DbTable/Friends.php
+++ application/models/DbTable/Friends.php
@@ -23,4 +23,73 @@ class Application_Model_DbTable_Friends extends Application_Model_DbTable_Abstra
         }
 
     }
+
+    public function addFriend($friendId) {
+        $status = false;
+        $userId = Zend_Auth::getInstance()->getIdentity()->id;
+
+        $isInList = $this->isInList($friendId);
+        $userTable = new Application_Model_DbTable_Users();
+        $friend = $userTable->getItem($friendId);
+        $accountPage = $this->baseLink . "/user/{$userId}";
+        if($isInList){
+            if($isInList['recipient_status'] == 0 && $isInList['sender_status'] == 1) {
+                $where = $this->getAdapter()->quoteInto('id = ?', (int)$isInList['id']);
+                $data = array(
+                    'recipient_status' => 1,
+                    'updated_at' => date('Y-m-d H:m:s')
+                );
+                $status = $this->update($data, $where);
+
+                $messageTable = new Application_Model_DbTable_Message();
+                $data = array(
+                    'sender_id' => $userId,
+                    'recipient_id' => $friendId,
+                    'content' => 'Hello ' . $friend["username"] . ', I have approved your request. My Account page is <a href="' . $accountPage . '">' . $friend["username"] . '</a>' ,
+                    'subject' => "Add to Friend Request",
+                );
+                $messageTable->sendMessage($data);
+            } else {
+                $status = false;
+            }
+        } else {
+            $data = array(
+                'sender_id' => $userId,
+                'friend_id' => $friendId,
+                'sender_status' => 1,
+                'recipient_status' => 0,
+                'created_at' => date('Y-m-d H:m:s'),
+                'updated_at' => date('Y-m-d H:m:s')
+            );
+
+            $status = $this->insert($data);
+
+            $messageTable = new Application_Model_DbTable_Message();
+            $data = array(
+                        'sender_id' => $userId,
+                        'recipient_id' => $friendId,
+                        'content' => 'Hello ' . $friend["username"] . ', I\'d like add you to my account. My Account page is <a href="' . $accountPage . '">' . $friend["username"] . '</a>' ,
+                        'subject' => "Add to Friend Request",
+                    );
+            $messageTable->sendMessage($data);
+        }
+
+        if ($status) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function isInList($friendId) {
+        $userId = Zend_Auth::getInstance()->getIdentity()->id;
+
+        $data = $this->select()
+            ->from($this->_name)
+            ->where('(' . $this->getAdapter()->quoteInto('sender_id=?' , $userId) . ' AND ' . $this->getAdapter()->quoteInto('friend_id=?' , $friendId) . ') OR (' . $this->getAdapter()->quoteInto('sender_id=?' , $friendId) . ' AND ' . $this->getAdapter()->quoteInto('friend_id=?' , $userId) . ')');
+        $result = $data->query()->fetch();
+
+        return $result;
+    }
 }
