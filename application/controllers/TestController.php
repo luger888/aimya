e9f65f83d95a91aa629dca73bf005bbc92051f25
Aleksandr Lozinskiy <alex.lozinskit@gmail.com>
2012-12-19 15:27:10 +0200

    added test data for paypal class

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -24,7 +24,7 @@ class TestController extends Zend_Controller_Action
         $doCheck = new Aimya_PayPal_Paypal();
         $isPaid = $doCheck->checkPayment($_POST);
         $dbSkills = new Application_Model_DbTable_ResumeSkills();
-        $doCheck->setLogFile('paypal.txt');
+        $doCheck->setLogFile('./paypal.txt');
         $data = array();
 
         if($isPaid == true){
