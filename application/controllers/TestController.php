896e647666aeb1a14856b70213fda0534dda5f8e
Aleksandr Lozinskiy <alex.lozinskit@gmail.com>
2012-12-19 20:01:14 +0200

    sdvb

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -36,5 +36,58 @@ class TestController extends Zend_Controller_Action
             $dbSkills->createSkill($data, 54);
         }
     }
+
+    public function responsenewAction()
+    {
+
+        ini_set('log_errors', true);
+        ini_set('error_log', dirname(__FILE__).'/ipn_errors.log');
+
+        $dbSkills = new Application_Model_DbTable_ResumeSkills();
+
+        $listener = new Aimya_PayPal_IpnListener();
+        // tell the IPN listener to use the PayPal test sandbox
+        $listener->use_sandbox = true;
+
+        // try to process the IPN POST
+        try {
+            $listener->requirePostMethod();
+            $verified = $listener->processIpn();
+        } catch (Exception $e) {
+            $this->writeLog($e->getMessage());
+            exit(0);
+        }
+
+        if($verified){
+            $this->writeLog("VALID IPN");
+            $this->writeLog($listener->getTextReport());
+            $data['skill']= 'success';
+            $dbSkills->createSkill($data, 53);
+        }else{
+            $this->writeLog("INVALID IPN");
+            $this->writeLog($listener->getTextReport());
+            $data['skill']= 'fail';
+
+            $dbSkills->createSkill($data, 54);
+        }
+    }
+
+    public function writeLog($data)
+    {
+
+        if( $fh = @fopen("./img/paypal.txt", "a+") )
+        {
+
+            $data = print_r($data, 1);
+            fwrite($fh, $data);
+            fclose( $fh );
+            return( true );
+        }
+        else
+        {
+            return( false );
+        }
+
+    }
 }
 
--- library/Aimya/PayPal/Paypal.php
+++ library/Aimya/PayPal/Paypal.php
@@ -172,11 +172,11 @@ class Aimya_PayPal_Paypal
         }
         elseif(strpos($curl_result, "INVALID")!==false) {
             $this->doLog("bla bla bla");
-            $this->doLog($curl_err);
+            $this->doLog($curl_result);
             return false;
         } else {
             $this->doLog("lab lab lab");
-            $this->doLog($curl_err);
+            $this->doLog($curl_result);
             return false;
         }
 
