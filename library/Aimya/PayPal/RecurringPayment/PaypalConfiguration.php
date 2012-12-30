62956c6ac16d0d504cd003e975bc5f018dc9911c
Aleksandr Lozinskiy <alex.lozinskit@gmail.com>
2012-12-31 01:10:40 +0200

    case fix

--- library/Aimya/PayPal/RecurringPayment/PaypalConfiguration.php
+++ library/Aimya/PayPal/RecurringPayment/PaypalConfiguration.php
@@ -230,6 +230,7 @@ class Aimya_PayPal_RecurringPayment_PaypalConfiguration {
 	 * @return string PayPal in context payment checkout URL
 	 */
 	public static function checkout_url() {
+        die('000');
 		return ( self::$_cache['environment'] == 'sandbox' ) ? 'https://www.sandbox.paypal.com/incontext?token=' : 'https://www.paypal.com/incontext?token=';
 	}
 
