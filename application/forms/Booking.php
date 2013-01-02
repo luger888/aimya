95caec6e28defc4209001b9ee7c34aa10e009ff8
luger888 <@DimaSvitlaWin7.(none)>
2013-01-02 15:25:38 +0200

    reworking booking form(adding time)

--- application/forms/Booking.php
+++ application/forms/Booking.php
@@ -55,10 +55,16 @@ class Application_Form_Booking extends Zend_Form
         $start_at ->setAttrib('id', 'started_at')
             ->setRequired(true)
             ->addValidator('NotEmpty')
-            ->setAttrib('class', 't-165')
             ->addFilters($this->basicFilters)
             ->setDecorators($this->basicDecorators);
 
+        $start_at_time = new Zend_Form_Element_Text('started_at_time');
+        $start_at_time ->setAttrib('id', 'started_at_time')
+//            ->setRequired(true)
+//            ->addValidator('NotEmpty')
+            ->setAttrib('class','hasTimepicker')
+            ->addFilters($this->basicFilters)
+            ->setDecorators($this->basicDecorators);
 
         $focus_name = new Zend_Form_Element_Text('focus_name');
         $focus_name ->setAttrib('id', 'focus_name')
@@ -120,7 +126,7 @@ class Application_Form_Booking extends Zend_Form
     }
 
 
-        $this->addElements(array($role, $recipiend_id,  $start_at,  $focus_name, $rate, $duration, $video, $feedback, $notes, $info, $submit ));
+        $this->addElements(array($role, $recipiend_id,  $start_at, $start_at_time,  $focus_name, $rate, $duration, $video, $feedback, $notes, $info, $submit ));
 
     }
 }
