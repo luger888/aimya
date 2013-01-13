e2c7fb371c87e6ef1f5c345f6c44fb5efe441a09
Dima Ivanov <morbes77@gmail.com>
2013-01-13 13:39:07 +0200

    admin add/remove multi categories backend/frontend

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -5,11 +5,35 @@ class AdminController extends Zend_Controller_Action
     public function init()
     {
         $this->_helper->layout->setLayout("layoutInner");
+        $this->view->headScript()->appendFile('../../../js/jquery/admin/admin.js');
+        $this->_helper->AjaxContext()
+            ->addActionContext('index', 'json')
+            ->initContext('json');
+
+
     }
 
     public function indexAction()
     {
+        $lessonCatDb = new Application_Model_DbTable_LessonCategory();
+        $categories = $lessonCatDb->getLessonCategories();
+        $this->view->categories = $categories;
 
+        if ($this->getRequest()->isPost()) {
+
+            if ($this->getRequest()->getParam('deleteId')) {
+                $result = $lessonCatDb->removeCat($this->getRequest()->getParam('deleteId'));
+                if ($result) {
+                    $this->view->result = 1;
+                } else {
+                    $this->view->fail = 1;
+                }
+            }
+
+            if ($this->getRequest()->getParam('categories')) {
+                 $lessonCatDb->addCats($this->getRequest()->getParams());
+            }
+        }
     }
 
     public function usersAction()
--- application/models/DbTable/LessonCategory.php
+++ application/models/DbTable/LessonCategory.php
@@ -6,10 +6,39 @@ class Application_Model_DbTable_LessonCategory extends Application_Model_DbTable
     public function getLessonCategories(){
 
         $data = $this   ->select()
-            ->from('lesson_category', (array('title')));
+            ->from('lesson_category', (array('id', 'title', 'status')));
 
         return $data->query()->fetchAll();
 
     }
+    public function removeCat($id)
+    {
+        $data = array(
+            'status' => 0,
+            'updated_at' => date('Y-m-d H:i:s')
 
+        );
+        $where = array(
+            $this->getAdapter()->quoteInto('id=?', $id)
+
+        );
+        return $this->update($data, $where);
+
+    }
+
+    public function  addCats($cats){
+
+        for($i=0;$i<count($cats['categories']);$i++){
+            $data = array(
+                'title' => $cats['categories'][$i],
+                'status' => 1,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+
+            );
+
+            $this->insert($data);
+        }
+
+    }
 }
