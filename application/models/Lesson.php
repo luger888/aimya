1285a29b8099e1ebdc873a73f601d9959d282ca1
Aleksandr Lozinskiy <alex.lozinskit@gmail.com>
2013-01-14 22:48:21 +0200

    added script to open display and run selenium server

--- application/models/Lesson.php
+++ application/models/Lesson.php
@@ -45,6 +45,42 @@ class Application_Model_Lesson
         return $notePath;
     }
 
+    public function createVideoPath($lessonId, $teacherId)
+    {
+
+        @mkdir(realpath(APPLICATION_PATH . '/../public/') . DIRECTORY_SEPARATOR . 'users');
+        @mkdir(realpath(APPLICATION_PATH . '/../public/') . DIRECTORY_SEPARATOR . 'users' . DIRECTORY_SEPARATOR . $teacherId);
+        @mkdir(realpath(APPLICATION_PATH . '/../public/') . DIRECTORY_SEPARATOR . 'users' . DIRECTORY_SEPARATOR . $teacherId . DIRECTORY_SEPARATOR . $lessonId);
+        @mkdir(realpath(APPLICATION_PATH . '/../public/') . DIRECTORY_SEPARATOR . 'users' . DIRECTORY_SEPARATOR . $teacherId . DIRECTORY_SEPARATOR . $lessonId . DIRECTORY_SEPARATOR . 'video');
+        $notePath = realpath(APPLICATION_PATH . '/../public/') . DIRECTORY_SEPARATOR . 'users' . DIRECTORY_SEPARATOR . $teacherId . DIRECTORY_SEPARATOR . $lessonId . DIRECTORY_SEPARATOR . 'video' . DIRECTORY_SEPARATOR;
+        @mkdir($notePath);
+
+        //$this->write(' / ' . $identityId . " / \n");
+
+        return $notePath;
+    }
+
+    public function openDisplay()
+    {
+        passthru("open_disp.sh 3", $display);
+        print_r($display);
+        die;
+
+        return $display;
+    }
+
+    public function closeDisplay()
+    {
+        $result = exec('close_display.sh');
+        return $result;
+    }
+
+    public function startRecording()
+    {
+        $result = exec('start_recording.sh');
+        return $result;
+    }
+
     public function createNote($notePath, $userName, $message, $time)
     {
         $string = '<li class="note"><p class="username">' . $userName . '</p><p class="time">' . $time . '</p><p class="message">' . $message  . '</p></li>';
