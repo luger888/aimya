504193065d9bdcc52ff4fd9def431d9644ef24f1
Aleksandr Lozinskiy <alex.lozinskit@gmail.com>
2013-01-29 12:59:53 +0200

    temp changes for subscription

--- application/controllers/PaymentController.php
+++ application/controllers/PaymentController.php
@@ -201,7 +201,7 @@ class PaymentController extends Zend_Controller_Action implements Aimya_Controll
 
             $aimyaProfit = $period * $this->subscriptionCost;
 
-            $lastId = $subscriptionTable->lastInsertId();
+            $lastId = $subscriptionTable->getAdapter()->lastInsertId();
 
             var_dump($lastId);
             die;
