79d2f1a277ab4254197f6c8aa66ac869ec02fb16
Aleksandr Lozinskiy <alex.lozinskit@gmail.com>
2013-01-29 12:27:44 +0200

    temp changes for subscription

--- application/models/DbTable/Subscriptions.php
+++ application/models/DbTable/Subscriptions.php
@@ -172,14 +172,14 @@ class Application_Model_DbTable_Subscriptions extends Application_Model_DbTable_
         return $data->query()->fetch();
     }
 
-    public function updateSubscriptionStatus($userId, $subscriptionId) {
+    public function updateSubscriptionStatus($subscriptionId) {
 
         $data = array(
             'status' => 'paid',
             'updated_at' => date('Y-m-d H:i:s')
         );
 
-        $where[] = $this->getAdapter()->quoteInto('user_id=?', (int)$userId);
+        //$where[] = $this->getAdapter()->quoteInto('user_id=?', (int)$userId);
         $where[] = $this->getAdapter()->quoteInto('id=?', (int)$subscriptionId);
 
         $this->update($data , $where);
