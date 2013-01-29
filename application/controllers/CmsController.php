2936be3454cfa0f47c6066920aa073857446980d
luger888 <@DimaSvitlaWin7.(none)>
2013-01-29 12:27:39 +0200

    main page cms

--- application/controllers/CmsController.php
+++ application/controllers/CmsController.php
@@ -13,9 +13,12 @@ class CmsController extends Aimya_Controller_BaseController
             ->addActionContext('delete', 'json')
             ->addActionContext('view', 'json')
             ->initContext('json');
+        $this->view->headScript()->appendFile('../../js/jquery/validation/registrationValidation.js');
         $login = new Application_Form_Login();
+        $reg = new Application_Form_Registration();
 
         $this->view->login = $login->getElements();
+        $this->view->reg = $reg->getElements();
     }
 
     public function indexAction()
