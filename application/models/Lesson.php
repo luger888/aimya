bdc50da87097c7392f20ab138d1a671712c2d69e
Aleksandr Lozinskiy <alex.lozinskit@gmail.com>
2013-02-05 14:06:22 +0200

    temp changes for audio recording

--- application/models/Lesson.php
+++ application/models/Lesson.php
@@ -81,9 +81,8 @@ class Application_Model_Lesson
         $time = gmdate("H:i:s", $seconds);
         exec("phase2_rec.sh $display $path $time > /dev/null 2>/dev/null &");
         $res = exec("/usr/local/bin/phase2.1_rtmpdump.sh $teacherStream $path > /dev/null 2>/dev/null &");
-        echo $res . '<br>';
-        var_dump("/usr/local/bin/phase2.1_rtmpdump.sh $teacherStream $path > /dev/null 2>/dev/null &");
-        die;
+        $this->write($res . '<br>');
+        $this->write("/usr/local/bin/phase2.1_rtmpdump.sh $teacherStream $path > /dev/null 2>/dev/null &");
         return true;
     }
 
