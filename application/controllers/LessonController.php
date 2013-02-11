b4204cab0b2990c899e65da47ab7ffd580cb8ad8
luger888 <morbes77@gmail.com>
2013-02-11 18:23:47 +0200

    minor fixes and updates

--- application/controllers/LessonController.php
+++ application/controllers/LessonController.php
@@ -39,8 +39,7 @@ class LessonController extends Zend_Controller_Action
         $extendedBookingList = $lessonModel->extendLesson($bookingList);
         $this->view->availableLesson = $lesson;
         $this->view->bookingList = $extendedBookingList;
-        $activeLesson = $lessonTable->checkAvailableLesson(Zend_Auth::getInstance()->getIdentity()->id);
-        Zend_Debug::dump($activeLesson);die;
+
 
     }
 
