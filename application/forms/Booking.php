7f7cf31e23024031da7d2641301644949ab44b01
Dima Ivanov <morbes77@gmail.com>
2013-03-20 10:28:49 +0200

    minor fixes

--- application/forms/Booking.php
+++ application/forms/Booking.php
@@ -65,6 +65,7 @@ class Application_Form_Booking extends Zend_Form
             ->setRequired(true)
             ->addValidator('NotEmpty')
             ->setAttrib('class','hasTimepicker')
+            ->setErrorMessages(array('Please specify a time'))
             ->setAttrib('autocomplete','off')
             ->addFilters($this->basicFilters)
             ->setDecorators($this->basicDecorators);
