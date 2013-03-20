ae478fc54048528119dce1c0c24b848d9d9f0437
unknown <luger@DimaSvitlaWin7.(none)>
2013-03-20 16:34:35 +0200

    word uploading

--- application/forms/Presentation.php
+++ application/forms/Presentation.php
@@ -14,7 +14,7 @@ class Application_Form_Presentation extends Zend_Form
         $presentation ->setLabel('Upload Image')
              ->setAttrib('id', 'Filedata')
              ->addValidator('Size', false, 25024000)
-             ->addValidator('Extension', false, 'ppt,pdf,pptx');
+             ->addValidator('Extension', false, 'ppt,pdf,pptx,doc,docx');
              //->setDestination('./flash/presentation/'.$identity->id.'/1/');
 
 
